c563f74260519b45080d0343521d5bcc8e70346e
1547784788 <1547784788@qq.com>
2020-06-12 14:59:18 +0800

    2020.6.12

--- DeepNeuralNetwork/DeepNeuralNetwork.cpp
+++ DeepNeuralNetwork/DeepNeuralNetwork.cpp
@@ -1,11 +1,46 @@
 ﻿// DeepNeuralNetwork.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
+// dip-class-demos.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <opencv.hpp>
+#include "function.h"
+#include "parameters.h"
+
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//开始计时
+	double start = static_cast<double>(getTickCount());
+
+	int method = DEMO_METHOD;
+
+	if (method == 0) {
+		yoloV3();
+	}
+	else if (method == 1) {
+		openpose();
+	}
+
+	//结束计时
+	double time = ((double)getTickCount() - start) / getTickCount();
+	//显示时间
+	cout << "processing time:" << time / 1000 << "ms" << endl;
+
+	//等待键盘响应，按任意键结束程序
+	system("pause");
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
---
+++ DeepNeuralNetwork/parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		0			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"D:\\DigitalImage\\Opencv4.0\\opencv\\sources\\samples\\data\\vtest.avi"		
+#define OPENPOSE_VIDEO		/*"H:\\openposeTest.mp4"	*/"E:\\课程\\大二下\\数字图像处理\\开合跳_55个.mp4"	
+
+//#define INPUT_METHOD									//0: image 1:video 2:camera
